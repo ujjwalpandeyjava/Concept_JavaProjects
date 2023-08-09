4d25c1604d5d713c3f24a70fa2519951bd4990c2
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2023-08-09 21:34:49 +0530

    more.

---
+++ Additional_Practice/src/main/java/UtilityClassExample/MathUtils.java
@@ -0,0 +1,12 @@
+public final class MathUtils {
+	private MathUtils() { // private constructor, will not allow instance creation.	// will throw error constructor not visible
+	}
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static int subtract(int a, int b) {
+		return a - b;
+	}
+}
