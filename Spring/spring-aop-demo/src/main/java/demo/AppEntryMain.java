cbf713ef8fc193796eef55fed0ef7895ff19e056
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2023-10-14 12:22:57 +0530

    some updates.

--- Spring/spring-aop-demo/src/main/java/demo/AppEntryMain.java
+++ Spring/spring-aop-demo/src/main/java/demo/AppEntryMain.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 
-public class Main {
+public class AppEntryMain {
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
 		var shopCart = context.getBean(ShoppingCart.class);
