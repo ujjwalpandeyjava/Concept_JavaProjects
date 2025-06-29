772070484f18b95164e584a66d959addd2c9f175
ujjwalpandeyjava <ujjwalpandey.aps@gmail.com>
2025-06-30 00:04:28 +0530

    Added spring boot project to keep examples of spring boot modules.

---
+++ SpringbootMoudles/src/main/java/concept/explorer/SpringbootMoudles/SpringbootMoudlesApplication.java
@@ -0,0 +1,13 @@
+package concept.explorer.SpringbootMoudles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMoudlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMoudlesApplication.class, args);
+	}
+
+}
---
+++ SpringbootMoudles/src/test/java/concept/explorer/SpringbootMoudles/SpringbootMoudlesApplicationTests.java
@@ -0,0 +1,13 @@
+package concept.explorer.SpringbootMoudles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMoudlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
